cca467e07ef26fbfcb0c6a21f3e6c3e41fa6e34b
Cenk Alti <cenkalti@gmail.com>
2016-10-09 12:57:30 +0300

    first version

---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='tus.py',
+    description='tus (resumable file upload protocol) client',
+    version='0.0.0',
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    url='http://github.com/cenk/tus.py',
+    py_modules=['tus'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'tus-upload = tus:_cmd_upload',
+            'tus-resume = tus:_cmd_resume',
+        ],
+    },
+)
---
+++ tus.py
@@ -0,0 +1,174 @@
+import os
+import argparse
+import logging
+from base64 import b64encode
+
+import requests
+
+LOG_LEVEL = logging.INFO
+DEFAULT_CHUNK_SIZE = 4 * 1024 * 1024
+TUS_VERSION = '1.0.0'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class TusError(Exception):
+    pass
+
+
+def _init():
+    fmt = "[%(asctime)s] %(levelname)s %(message)s"
+    h = logging.StreamHandler()
+    h.setLevel(LOG_LEVEL)
+    h.setFormatter(logging.Formatter(fmt))
+    logger.addHandler(h)
+
+
+def _create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('rb'))
+    parser.add_argument('--chunk-size', default=DEFAULT_CHUNK_SIZE)
+    parser.add_argument(
+        '--authorization',
+        help="Authorization header value to be sent with requests")
+    return parser
+
+
+def _cmd_upload():
+    _init()
+
+    parser = _create_parser()
+    parser.add_argument('tus_endpoint')
+    parser.add_argument('--file_name')
+    args = parser.parse_args()
+
+    upload(
+        args.file,
+        args.tus_endpoint,
+        chunk_size=args.chunk_size,
+        file_name=args.file_name,
+        authorization=args.authorization,
+    )
+
+
+def _cmd_resume():
+    _init()
+
+    parser = _create_parser()
+    parser.add_argument('file_endpoint')
+    args = parser.parse_args()
+
+    resume(
+        args.file,
+        args.file_endpoint,
+        chunk_size=args.chunk_size,
+        authorization=args.authorization,
+    )
+
+
+def upload(
+        file_obj,
+        tus_endpoint,
+        chunk_size=DEFAULT_CHUNK_SIZE,
+        file_name=None,
+        authorization=None):
+    file_name = os.path.basename(file_obj.name)
+    file_size = _get_file_size(file_obj)
+    location = _create_file(
+            tus_endpoint,
+            file_name,
+            file_size,
+            authorization=authorization)
+    resume(
+            file_obj,
+            location,
+            chunk_size=chunk_size,
+            authorization=authorization)
+
+
+def _get_file_size(f):
+    pos = f.tell()
+    f.seek(0, 2)
+    size = f.tell()
+    f.seek(pos)
+    return size
+
+
+# TODO file_name alma, metadata al
+def _create_file(tus_endpoint, file_name, file_size, authorization=None):
+    logger.info("Creating file endpoint")
+
+    headers = {
+        "Tus-Resumable": TUS_VERSION,
+        "Upload-Length": str(file_size),
+        "Upload-Metadata": "name %s" % b64encode(file_name),
+    }
+
+    if authorization:
+        headers["Authorization"] = authorization
+
+    response = requests.post(tus_endpoint, headers=headers)
+    if response.status_code != 201:
+        raise TusError("Create failed: %s" % response)
+
+    location = response.headers["Location"]
+    logger.info("Location: %s", location)
+    return location
+
+
+def resume(
+        file_obj,
+        file_endpoint,
+        chunk_size=DEFAULT_CHUNK_SIZE,
+        authorization=None):
+    file_size = _get_file_size(file_obj)
+    offset = _get_offset(file_endpoint, authorization=authorization)
+    while offset < file_size:
+        file_obj.seek(offset)
+        data = file_obj.read(chunk_size)
+        offset = _upload_chunk(data, offset, file_endpoint,
+                               authorization=authorization)
+
+
+def _get_offset(file_endpoint, authorization=None):
+    logger.info("Getting offset")
+
+    headers = {
+        "Tus-Resumable": TUS_VERSION,
+    }
+
+    if authorization:
+        headers["Authorization"] = authorization
+
+    response = requests.head(file_endpoint,  headers=headers)
+    response.raise_for_status()
+
+    offset = _extract_offset(response)
+    logger.info("Offset: %s", offset)
+    return offset
+
+
+def _upload_chunk(data, offset, file_endpoint, authorization=None):
+    logger.info("Uploading chunk")
+
+    headers = {
+        'Content-Type': 'application/offset+octet-stream',
+        'Upload-Offset': str(offset),
+        'Tus-Resumable': TUS_VERSION,
+    }
+
+    if authorization:
+        headers['Authorization'] = authorization
+
+    response = requests.patch(file_endpoint, headers=headers, data=data)
+    if response.status_code != 204:
+        raise TusError("Upload chunk failed: %s" % response)
+
+    offset = _extract_offset(response)
+    logger.info("Offset: %s", offset)
+    return offset
+
+
+def _extract_offset(response):
+    return int(response.headers["Upload-Offset"])
