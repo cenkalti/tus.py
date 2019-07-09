73437c8f5f26a3da854575a80554ab4e2462b433
Cenk Alti <cenkalti@gmail.com>
2019-07-09 15:47:53 +0300

    bump patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='tus.py',
     description='tus (resumable file upload protocol) client',
-    version='1.3.3',
+    version='1.3.4',
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     url='https://github.com/cenk/tus.py',
